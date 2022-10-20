374670f85bca9f1821d1ed01cda01ae631447867
Electr0Nick <kozhakin.maks@gmail.com>
2022-10-20 12:08:58 +0300

    add js for hide time blocks

--- js/script-block.js
+++ js/script-block.js
@@ -1,43 +1,60 @@
 "use strict"
 
-let infoBlock = document.getElementById('advantage');
-let infoBlockWidth = infoBlock.offsetWidth;
-// console.log(infoBlock);
-// console.log(infoBlockWidth);
-
-let timeBlock = document.getElementById('timeblock');
+let timeBlock = document.querySelector(".info__timesblock");
 let timeBlockWidth = timeBlock.offsetWidth;
-// console.log(timeBlock);
-console.log(timeBlockWidth);
-
-// --------------------------------------------------------------------------
-let timeBlockWidth1 = 347; // получаем автоматически длину блока timeblock
-
-let oneStep = 64 + 7; // получаем автоматически длину time + gap
-let remainder = timeBlockWidth1; // разница (изначально приравниваем к длине блока)
-let counter; // создаём счётчик
+let timeWidth = document.querySelector(".info__time").offsetWidth;
+let timeBlockStyles = window.getComputedStyle(timeBlock);
+let gap = parseInt(timeBlockStyles.gap);
+let oneStep = timeWidth + gap;
+let remainder = timeBlockWidth;
+let capacity;
+let arrayTimeBlock = document.querySelectorAll(".info__timesblock");
 
 for (let i = 1; i < 100; i++) {
-    remainder -= oneStep; // каждый раз вычитаем из разницы шаг
-    if (remainder < oneStep - 7) { // если разница станет меньше чем шаг - gap
-        counter = i; // обновляем счётчик
-        break; // завершаем
+    remainder -= oneStep;
+    if (remainder < oneStep - gap) {
+        capacity = i;
+        break;
     }
 }
 
-// получаем количество элементов time
+function hideAndOpenAllTimes(timeBlock) {
+    let arrayTime = timeBlock.querySelectorAll(".info__time");
+    let arrayTimeLength = arrayTime.length;
+    let moreBtn = timeBlock.querySelector(".info__more");
+    let closeBtn = timeBlock.querySelector(".info__close");
 
-// получаем массив всех элементов time
+    function hideTimes() {
+        moreBtn.classList.remove('displaynone');
+        closeBtn.classList.add('displaynone');
+        for (let i = 1; i < arrayTimeLength; i++) {
+            if (i >= capacity - 1) {
+                arrayTime[i].classList.add('displaynone');
+            }
+        }
+    }
+    function openTimes() {
+        moreBtn.classList.add('displaynone');
+        closeBtn.classList.remove('displaynone');
+        for (let i = 0; i < arrayTimeLength; i++) {
+            if (arrayTime[i].classList.contains('displaynone')) {
+                arrayTime[i].classList.remove('displaynone');
+            }
+        }
+    }
 
-// если счетчик == количество time то ок
+    if (capacity < arrayTimeLength) {
+        hideTimes();
+    }
+    
+    moreBtn.addEventListener('click', function () {
+        openTimes();
+    });
+    closeBtn.addEventListener('click', function () {
+        hideTimes();
+    });
+}
 
-// если меньше
-// включаем элемент ЕЩЁ
-// перебираем for массив элементов time 
-// если i больше счетчика, добавляем элементу display = "none"
-
-let test = document.querySelectorAll(".info__time")[3];
-test.style.display = "none";
-
-// вешаем на элемент ЕЩЁ обработчик click
-// при нажатии элемент ЕЩЁ выключаем, все остальные элементы включаем
+for (let i = 0; i < arrayTimeBlock.length; i++) {
+    hideAndOpenAllTimes(arrayTimeBlock[i]);
+}
